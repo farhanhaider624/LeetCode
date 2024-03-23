cff2799b75c1d413491006feb40a573439feaf25
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-03-23 08:44:21 +0530

    Time: 4 ms (82.48%), Space: 9.3 MB (96.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,29 @@
 class Solution {
 private:
-    int func(string &s, string &t, int i, int j, vector<vector<int>> &dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
+//     int func(string &s, string &t, int i, int j, vector<vector<int>> &dp){
+//         if(i==0) return j+1;
+//         if(j==0) return i+1;
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(s[i]==t[j]) return dp[i][j]= func(s, t, i-1, j-1, dp);
-        return dp[i][j]=1+min(func(s, t, i, j-1, dp) ,min(func(s, t, i-1, j, dp),func(s, t, i-1, j-1, dp)));
-    }
+//         if(s[i-1]==t[j-1]) return dp[i][j]= func(s, t, i-1, j-1, dp);
+//         return dp[i][j]=1+min(func(s, t, i, j-1, dp) ,min(func(s, t, i-1, j, dp),func(s, t, i-1, j-1, dp)));
+//     }
 public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        if(n==0 || m==0){
-            if(m!=0) return m;
-            if(n!=0) return n;
+
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        for(int j=0; j<=m; j++) prev[j]=j;
+        
+        for(int i=1; i<=n; i++){
+            curr[0]=i;
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]) curr[j]= prev[j-1];
+                else curr[j]=1+min(curr[j-1] ,min(prev[j], prev[j-1]));
+            }
+            prev=curr;
         }
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return func(word1, word2, n-1, m-1, dp);
+        return prev[m];
     }
 };
