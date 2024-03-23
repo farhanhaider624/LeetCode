85c474c2cd998bf481c90a972ab245464243dd9b
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-03-23 22:30:31 +0530

    Time: 46 ms (51.15%), Space: 11.2 MB (88.58%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -19,9 +19,19 @@ public:
     bool isMatch(string s, string p) {
         int n=p.size();
         int m=s.size();
-        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0]=true;
-        for(int j=1; j<=m; j++) dp[0][j]=false;
+        vector<bool> prev(m+1, false), curr(m+1, false);
+        prev[0]=true;
+        for(int j=1; j<=m; j++) prev[j]=false;
+        // for(int i=1; i<=n; i++){
+        //     bool flag=true;
+        //     for(int k=1; k<=i; k++){
+        //         if(p[k-1]!='*'){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+        //     curr[0]=flag;
+        // }
         for(int i=1; i<=n; i++){
             bool flag=true;
             for(int k=1; k<=i; k++){
@@ -30,17 +40,16 @@ public:
                     break;
                 }
             }
-            dp[i][0]=flag;
-        }
-        for(int i=1; i<=n; i++){
+            curr[0]=flag;
             for(int j=1; j<=m; j++){
                 //dono equal or '?' ho to
-                if(p[i-1]==s[j-1] || p[i-1]=='?') dp[i][j]=dp[i-1][j-1];
+                if(p[i-1]==s[j-1] || p[i-1]=='?') curr[j]=prev[j-1];
                 //jb * ho
-                else if(p[i-1]=='*') dp[i][j]=dp[i][j-1] || dp[i-1][j];
-                else dp[i][j]=false;
+                else if(p[i-1]=='*') curr[j]=curr[j-1] || prev[j];
+                else curr[j]=false;
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
