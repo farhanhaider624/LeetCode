ddf7c9838155ffd1aa3c29bcfddf9a66cad6fed2
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-04-01 02:44:39 +0530

    Time: 516 ms (13.10%), Space: 294.6 MB (16.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,12 +5,12 @@ public:
         if(ind==nums.size()) return 0;
         if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
         //not take
-        dp[ind][prev_ind+1]=0+f(ind+1, prev_ind, nums, dp);
+        int len=0+f(ind+1, prev_ind, nums, dp);
         //take
         if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            dp[ind][prev_ind+1]=max(dp[ind][prev_ind+1], 1+f(ind+1, ind, nums, dp));
+            len=max(len, 1+f(ind+1, ind, nums, dp));
         }
-        return dp[ind][prev_ind+1];
+        return dp[ind][prev_ind+1]=len;
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
