b223e6ef3f040223342873b32dd2d545ab65c0e8
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-07-30 19:00:29 +0530

    Time: 927 ms (18.69%), Space: 179.2 MB (27.89%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-    typedef pair<int, int> p;
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<pair<int, int>> adj[n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i!=j){
-                    int wt = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
-                    adj[i].push_back({j, wt});
-                }
-            }
+vector<int> parent;
+    vector<int> rank;
+    int find(int i){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i]);
+    }
+    void Union(int x, int y){
+        int xp = find(x);
+        int yp = find(y);
+        if(xp==yp) return;
+        if(rank[xp]>rank[yp]) parent[yp]=xp;
+        if(rank[xp]<rank[yp]) parent[xp]=yp;
+        else{
+            parent[yp]=xp;
+            rank[xp]++;
         }
-        vector<int> vis(n, 0);
-        priority_queue<p, vector<p>, greater<p>> pq;
-        pq.push({0, 0});
-        int sum=0;
-        while(!pq.empty()){
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            if(vis[node]) continue;
-            vis[node]=1;
-            sum+=wt;
-            for(auto &it: adj[node]){
-                int adjNode = it.first;
-                int w = it.second;
-                if(!vis[adjNode]){
-                    pq.push({w, adjNode});
-                }
+    }
+    bool static comparator(vector<int> &v1, vector<int> &v2){
+        return v1[2]<v2[2];
+    }
+    int Kruskals(vector<vector<int>> &vec){
+        int sum = 0;
+        for(auto &it:vec){
+            int p1 = find(it[0]);
+            int p2 = find(it[1]);
+            int wt = it[2];
+            if(p1!=p2){
+                Union(it[0], it[1]);
+                sum+=wt;
             }
         }
         return sum;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        parent.resize(n);
+        rank.resize(n, 0);
+        for(int i=0; i<n; i++) parent[i]=i;
+        vector<vector<int>> vec;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                int wt = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                vec.push_back({i, j, wt});
+            }
+        }
+        sort(vec.begin(), vec.end(), comparator);
+        return Kruskals(vec);
     }
 };
