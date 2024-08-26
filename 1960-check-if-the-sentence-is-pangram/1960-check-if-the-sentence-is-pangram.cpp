c60445e3c1e188955c8d3d2bcc2cc62917336bf9
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-08-27 03:34:55 +0530

    Time: 0 ms (100%), Space: 8 MB (55.57%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        unordered_map<char,int> mp;
-        for(char ch:sentence) mp[ch]++;
-        return (mp.size()==26);
+        vector<int> arr(26, 0);
+        for(char ch:sentence){
+            arr[ch-'a']=1;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i]==0) return false;
+        }
+        return true;
     }
 };
