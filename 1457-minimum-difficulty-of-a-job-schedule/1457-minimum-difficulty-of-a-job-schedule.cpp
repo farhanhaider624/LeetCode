b9bc0e9ce33a147983fc9a6a88cdf8696e57519a
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-09-02 23:13:46 +0530

    Time: 16 ms (82.29%), Space: 10.5 MB (11.98%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    int func(int ind, int d, int n, vector<int> &jd, vector<vector<int>> &dp){
-        //base case
-        if(d==1){
-            return *max_element(begin(jd)+ind, end(jd));
+    // int func(int ind, int d, int n, vector<int>& jd, vector<vector<int>>& dp) {
+    //     // base case
+    //     if (d == 1) {
+    //         return *max_element(begin(jd) + ind, end(jd));
+    //     }
+    //     if (dp[ind][d] != -1)
+    //         return dp[ind][d];
+    //     int maxD = jd[ind];
+    //     int finalResult = INT_MAX;
+    //     for (int i = ind; i <= n - d; i++) {
+    //         maxD = max(maxD, jd[i]);
+    //         int result = maxD + func(i + 1, d - 1, n, jd, dp);
+    //         finalResult = min(finalResult, result);
+    //     }
+    //     return dp[ind][d] = finalResult;
+    // }
+    int minDifficulty(vector<int>& jd, int day) {
+        int n = jd.size();
+        if (n < day)
+            return -1;
+        vector<vector<int>> dp(301, vector<int>(11, 0));
+
+        // base case
+        for (int ind = 0; ind < n; ind++) {
+            dp[ind][1] = *max_element(begin(jd) + ind, end(jd));
         }
-        if(dp[ind][d]!=-1) return dp[ind][d];
-        int maxD=jd[ind];
-        int finalResult = INT_MAX;
-        for(int i=ind; i<=n-d; i++){
-            maxD = max(maxD, jd[i]);
-            int result = maxD + func(i+1, d-1, n, jd, dp);
-            finalResult = min(finalResult, result);
+
+        for (int d = 2; d<=day; d++) {
+            for (int ind = 0; ind<=n-d; ind++) {
+                int maxD = jd[ind];
+                int finalResult = INT_MAX;
+                for (int i = ind; i <= n - d; i++) {
+                    maxD = max(maxD, jd[i]);
+                    int result = maxD + dp[i + 1][d - 1];
+                    finalResult = min(finalResult, result);
+                }
+                dp[ind][d] = finalResult;
+            }
         }
-        return dp[ind][d]=finalResult;
-    }
-    int minDifficulty(vector<int>& jd, int d) {
-        int n=jd.size();
-        if(n<d) return -1;
-        vector<vector<int>> dp(301, vector<int>(11, -1));
-        return func(0, d, n, jd, dp);
+
+        return dp[0][day];
     }
 };
