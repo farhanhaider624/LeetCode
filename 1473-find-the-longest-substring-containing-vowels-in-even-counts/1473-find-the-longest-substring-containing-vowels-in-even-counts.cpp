8990376a50a0b5571e96c48f0d983768d16ab5cf
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-09-16 02:06:42 +0530

    Time: 768 ms (5.04%), Space: 21.2 MB (7.11%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -9,19 +9,19 @@ public:
         vector<int> state(5, 0);
         for(int i=0; i<n; i++){
             if(s[i]=='a'){
-                state[0]=(state[0]+1)%2;
+                state[0]=(state[0]^1);
             }
             else if(s[i]=='e'){
-                state[1]=(state[1]+1)%2;
+                state[1]=(state[1]^1);
             }
             else if(s[i]=='i'){
-                state[2]=(state[2]+1)%2;
+                state[2]=(state[2]^1);
             }
             else if(s[i]=='o'){
-                state[3]=(state[3]+1)%2;
+                state[3]=(state[3]^1);
             }
             else if(s[i]=='u'){
-                state[4]=(state[4]+1)%2;
+                state[4]=(state[4]^1);
             }
             currState="";
             for(int j=0; j<5; j++){
