03d73adaeb9d89cb121044ee5bde992a33f392a0
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-09-22 02:30:54 +0530

    Time: 953 ms (5%), Space: 190.7 MB (28.92%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
+    static bool comparator(vector<int>& v1, vector<int>& v2) {
+        if (v1[0] != v2[0])
+            return v1[0] < v2[0]; 
+        return v1[1] < v2[1];
+    }
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         vector<vector<int>> ans(2);
-        map<int, int> w, l;
-        for(auto it:matches){
-            w[it[0]]++;
-            l[it[1]]++;
+        sort(matches.begin(), matches.end(), comparator);
+        map<int, int> looser;
+        for (auto it : matches) {
+            cout << it[0] << " " << it[1] << endl;
+            looser[it[1]]++;
         }
-        for(auto it:w){
-            if(l.find(it.first)==l.end()) ans[0].push_back(it.first);
+        int temp=-1;
+        for (auto it : matches) {
+            if(it[0]==temp) continue;
+            if (looser.find(it[0]) == looser.end())
+                ans[0].push_back(it[0]);
+            temp=it[0];
         }
-        for(auto it:l){
-            if(it.second==1) ans[1].push_back(it.first);
+        for (auto it : looser) {
+            if (it.second == 1)
+                ans[1].push_back(it.first);
         }
         return ans;
     }
