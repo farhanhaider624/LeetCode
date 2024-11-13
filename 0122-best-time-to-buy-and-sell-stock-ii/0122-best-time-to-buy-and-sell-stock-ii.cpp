fac3c522f4197b1a06fa22d9bf0ffcd007136e1c
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-11-13 10:36:07 +0530

    Time: 5 ms (21.2%), Space: 18 MB (27.28%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
-private:
-    // int f(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp){
-    //     //base case
-    //     if(ind==prices.size()) return 0;
+public:
+    // int func(int ind, int buy, vector<int> &prices, vector<vector<int>> &dp){
+    //     if(ind==prices.size()){
+    //         return 0;
+    //     }
     //     if(dp[ind][buy]!=-1) return dp[ind][buy];
     //     int profit=0;
     //     if(buy){
-    //         profit=max(-prices[ind]+f(ind+1, 0, prices, dp), 0+f(ind+1, 1, prices, dp));
+    //         //take or not take
+    //         profit=max(-prices[ind]+func(ind+1, 0, prices, dp), func(ind+1, 1, prices, dp));
     //     }
-    //     else profit=max(prices[ind]+f(ind+1, 1, prices, dp), 0+f(ind+1, 0, prices, dp));
+    //     else{
+    //         profit=max(prices[ind]+func(ind+1, 1, prices, dp), func(ind+1, 0, prices, dp));
+    //     }
     //     return dp[ind][buy]=profit;
     // }
-public:
     int maxProfit(vector<int>& prices) {
+        int ind=0, buy=1;
         int n=prices.size();
-        vector<int> ahead(2, 0), curr(2, 0);
-
-        ahead[0]=ahead[1]=0;
-
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        dp[n][0]=0;
+        dp[n][1]=0;
         for(int ind=n-1; ind>=0; ind--){
             for(int buy=0; buy<=1; buy++){
                 int profit=0;
                 if(buy){
-                    profit=max(-prices[ind]+ahead[0], 0+ahead[1]);
+                    //take or not take
+                    profit=max(-prices[ind]+dp[ind+1][0], dp[ind+1][1]);
                 }
-                else profit=max(prices[ind]+ahead[1], 0+ahead[0]);
-                curr[buy]=profit;
+                else{
+                    profit=max(prices[ind]+dp[ind+1][1], dp[ind+1][0]);
+                }
+                dp[ind][buy]=profit;
             }
-            ahead=curr;
         }
-
-        return ahead[1];
+        return dp[0][1];
     }
 };
