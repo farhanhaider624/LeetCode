6b8fe758d8fc85215c20a2ad304d21762b783481
Farhan Haider <97431289+farhanhaider624@users.noreply.github.com>
2024-11-23 01:05:51 +0530

    Time: 592 ms (22.88%), Space: 287.5 MB (8.39%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    void selectionSort(vector<int> &nums){
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            int mini=i;
+            for(int j=i+1; j<n; j++){
+                if(nums[j]<nums[mini]){
+                    mini=j;
+                }
+            }
+            swap(nums[i], nums[mini]);
+        }
+    }
+    void bubbleSort(vector<int> &nums){
+        int n=nums.size();
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=i-1; j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j], nums[j+1]);
+                }
+            }
+        }
+    }
+    void insertionSort(vector<int> &nums){
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            int j=i;
+            while(j>0 && nums[j-1]>nums[j]){
+                swap(nums[j], nums[j-1]);
+                j--;
+            }
+        }
+    }
     void merge(vector<int> &nums, int low, int mid, int high){
         vector<int> temp;
         int left=low;
@@ -8,7 +40,8 @@ public:
             if(nums[left]<=nums[right]){
                 temp.push_back(nums[left]);
                 left++;
-            }else{
+            }
+            else{
                 temp.push_back(nums[right]);
                 right++;
             }
@@ -34,6 +67,9 @@ public:
     }
     vector<int> sortArray(vector<int>& nums) {
         int n=nums.size();
+        // selectionSort(nums);
+        // bubbleSort(nums);
+        // insertionSort(nums);
         mergeSort(nums, 0, n-1);
         return nums;
     }
